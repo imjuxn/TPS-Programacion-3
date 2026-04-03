7d31460cac911bde924a6660613cd62c908418f3
Juan Silva <juan.silva96@hotmail.com>
2026-04-03 12:27:32 -0300

    Create data.js

---
+++ Unidad 3/js/data.js
@@ -0,0 +1,45 @@
+// Array de Strings: categorías
+const categorias = ["Hamburguesas", "Pizzas", "Papas Fritas", "Bebidas"];
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Hamburguesa Triple",
+    descripcion: "Triple carne, cheddar derretido y bacon crocante.",
+    precio: 25000,
+    imagen: "./assets/burguer.jpg",
+    categoria: "Hamburguesas",
+  },
+  {
+    id: 2,
+    nombre: "Pizza Muzzarella",
+    descripcion: "Salsa casera, muzzarella y orégano fresco.",
+    precio: 18000,
+    imagen: "./assets/pizza.jpg",
+    categoria: "Pizzas",
+  },
+  {
+    id: 3,
+    nombre: "Papas Fritas XL",
+    descripcion: "Porción grande con sal y alioli casero.",
+    precio: 8000,
+    imagen: "./assets/papas.jpg",
+    categoria: "Papas Fritas",
+  },
+  {
+    id: 4,
+    nombre: "Coca-Cola 500ml",
+    descripcion: "Bebida fría, perfecta para acompañar tu pedido.",
+    precio: 3500,
+    imagen: "./assets/Coca.jpg",
+    categoria: "Bebidas",
+  },
+  {
+    id: 5,
+    nombre: "Hamburguesa Veggie",
+    descripcion: "Medallón de garbanzo, lechuga, tomate y mayonesa.",
+    precio: 22000,
+    imagen: "./assets/burguer.jpg",
+    categoria: "Hamburguesas",
+  },
+];
